806091ac22187115048642087b3f797698cdfd5e
nobodyxu <Jiahao_XU@outlook.com>
2020-08-30 23:57:41 +1000

    Implement syscall for x86-64 linux

---
+++ syscall/syscall.h
@@ -0,0 +1,13 @@
+#ifndef  __aspawn_syscall_syscall_H__
+# define __aspawn_syscall_syscall_H__
+
+# include <sys/syscall.h>
+# include <stdint.h>
+
+/**
+ * Rationale on why syscall takes long:
+ *  - https://stackoverflow.com/questions/35628927/what-is-the-type-of-system-call-arguments-on-linux
+ */
+long syscall(long syscall_number, long arg1, long arg2, long arg3, long arg4, long arg5, long arg6);
+
+#endif
