32ad6605a119b58ff0887f1f8ee8ee69da00f25d
nobodyxu <Jiahao_XU@outlook.com>
2020-09-01 15:21:11 +1000

    Done implementing aspawn

--- aspawn.c
+++ aspawn.c
@@ -4,6 +4,9 @@
 #include "cached_stack/cached_stack.h"
 #include "clone_internal/clone_internal.h"
 #include "create_pipe/create_pipe.h"
+#include "syscall/syscall.h"
+
+#include <string.h>
 
 #include <unistd.h>
 #include <pthread.h>
@@ -22,19 +25,49 @@ int cleanup_stacks(const struct stack_t *cached_stack)
     return cleanup_cached_stack_internal(cached_stack);
 }
 
+struct aspawn_child_args {
+    int pipefd[2];
+
+    int (*fn)(void *arg, int wirte_end_fd, void *on_stack_obj, size_t len);
+    void *arg;
+
+    size_t user_data_len;
+    char user_data[];
+};
+int aspawn_child(void *arg)
+{
+    struct aspawn_child_args *args = arg;
+
+    psys_close(args->pipefd[0]);
+    return args->fn(args->arg, args->pipefd[1], args->user_data, args->user_data_len);
+}
 int aspawn_impl(pid_t *pid, struct stack_t *cached_stack, size_t additional_stack_requirement, 
-                int (*fn)(void *arg), void *arg)
+                int (*fn)(void *arg, int wirte_end_fd, void *on_stack_obj, size_t len), void *arg,
+                void *obj_to_place_on_stack, size_t len)
 {
     int pipefd[2];
     int result = create_cloexec_pipe(pipefd);
     if (result < 0)
         return result;
 
-    result = allocate_stack(cached_stack, (32 * 1024) + additional_stack_requirement);
+    size_t objs_on_stack_len = sizeof(struct aspawn_child_args) + len;
+    result = allocate_stack(cached_stack, (32 * 1024) + additional_stack_requirement, objs_on_stack_len);
     if (result < 0)
         return result;
 
-    int new_pid = clone_internal(fn, arg, cached_stack);
+    struct stack_t stack = *cached_stack;
+    void *obj_addr = allocate_obj_on_stack(&stack, objs_on_stack_len);
+
+    struct aspawn_child_args *args = obj_addr;
+    args->pipefd[0] = pipefd[0];
+    args->pipefd[1] = pipefd[1];
+    args->fn = fn;
+    args->arg = arg;
+
+    args->user_data_len = len;
+    memcpy(args->user_data, obj_to_place_on_stack, len);
+
+    int new_pid = clone_internal(aspawn_child, args, &stack);
     if (new_pid < 0)
         return new_pid;
 
@@ -45,7 +78,8 @@ int aspawn_impl(pid_t *pid, struct stack_t *cached_stack, size_t additional_stac
     return pipefd[0];
 }
 int aspawn(pid_t *pid, struct stack_t *cached_stack, size_t additional_stack_requirement, 
-           int (*fn)(void *arg), void *arg)
+           int (*fn)(void *arg, int wirte_end_fd, void *on_stack_obj, size_t len), void *arg,
+           void *obj_to_place_on_stack, size_t len)
 {
     int oldstate;
     pthread_setcancelstate(PTHREAD_CANCEL_DISABLE, &oldstate);
@@ -55,7 +89,7 @@ int aspawn(pid_t *pid, struct stack_t *cached_stack, size_t additional_stack_req
     if (result < 0)
         goto fail_to_block_signal;
 
-    result = aspawn_impl(pid, cached_stack, additional_stack_requirement, fn, arg);
+    result = aspawn_impl(pid, cached_stack, additional_stack_requirement, fn, arg, obj_to_place_on_stack, len);
 
     sig_setmask(&oldset);
 
