de9e64a3b1e1bf453eb77877dbeb0af28b909758
nobodyxu <Jiahao_XU@outlook.com>
2020-09-06 23:01:16 +1000

    Improve API psys_clone3: Add param fn and arg

--- syscall/clone3.c
+++ syscall/clone3.c
@@ -1,5 +1,6 @@
 #include "clone3.h"
 #include "make_syscall.h"
+#include "syscall.h"
 
 #include <sys/syscall.h>
 
@@ -11,7 +12,36 @@ long invoke_syscall(long syscall_number, long arg1, long arg2, long arg3, long a
     __builtin_unreachable();
 }
 
-__attribute__((always_inline)) long psys_clone3(struct clone_args *cl_args, size_t size)
+long psys_clone3(struct clone_args *cl_args, size_t size, int (*fn)(void *arg), void *arg)
 {
-    return invoke_syscall(SYS_clone3, (long) cl_args, size, 0, 0, 0, 0);
+    typedef int (*fn_t)(void *arg);
+
+    register fn_t fn_reg __asm__ ("r11");
+    register void *arg_reg __asm__ ("r12");
+
+    fn_reg = fn;
+    arg_reg = arg;
+
+    // invoke_syscall is always inline, thus no need of worring about the affect of switching to a new stack,
+    // since no function return (req + pop) will be performed, and the return value is in the register.
+    //
+    // The compiler probably won't spoil the return value from the register to the stack.
+    int result = invoke_syscall(SYS_clone3, (long) cl_args, size, 0, 0, 0, 0);
+
+    // If this is the child
+    if (result == 0) {
+        __asm__ __volatile__ (
+	        // Clear the frame pointer.
+            // This is suggested by the API to be done to mark the outermost frame.
+            "xorl %%ebp, %%ebp"
+            :
+            :
+            : "ebp"
+        );
+
+        int exit_status = fn_reg(arg_reg);
+        pure_syscall2(SYS_exit, exit_status);
+    }
+
+    return result;
 }
--- syscall/clone3.h
+++ syscall/clone3.h
@@ -19,6 +19,6 @@ struct clone_args {
 
 # define CLONE_CLEAR_SIGHAND 0x100000000ULL /* Clear any signal handler and reset to SIG_DFL. */
 
-__attribute__((always_inline)) long psys_clone3(struct clone_args *cl_args, size_t size);
+long psys_clone3(struct clone_args *cl_args, size_t size, int (*fn)(void *arg), void *arg);
 
 #endif
