c54c79d7e214b9ef318c964b1b2702a8d1e48cb2
nobodyxu <Jiahao_XU@outlook.com>
2020-09-26 20:22:32 +1000

    Update run_benchmark.sh: Disable frequency scaling
    to archive more consistent benchmark

--- benchmark/run_benchmark.sh
+++ benchmark/run_benchmark.sh
@@ -1,1 +1,9 @@
-../test/run_tests.sh
+#!/bin/bash
+
+for each in $1; do
+    echo -e '\nRunning' $each ...
+    sudo cpupower frequency-set --governor performance
+    LD_LIBRARY_PATH=../ ./$each
+    sudo cpupower frequency-set --governor powersave
+    echo
+done
