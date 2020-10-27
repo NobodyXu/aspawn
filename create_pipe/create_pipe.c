39eb4c46b9333ebd9d888bd0de56632d9365c93e
nobodyxu <Jiahao_XU@outlook.com>
2020-10-27 19:00:54 +1100

    modify create_cloexec_pipe to use psys_pipe2

--- create_pipe/create_pipe.c
+++ create_pipe/create_pipe.c
@@ -3,13 +3,9 @@
 #include "create_pipe.h"
 
 #include <fcntl.h>
-#include <unistd.h>
-
-#include <errno.h>
+#include "../syscall/syscall.h"
 
 int create_cloexec_pipe(int pipefd[2])
 {
-    if (pipe2(pipefd, O_CLOEXEC) < 0)
-        return -errno;
-    return 0;
+    return psys_pipe2(pipefd, O_CLOEXEC);
 }
