a44cf8cb4741ddd577377ead7424619bc7ff2a75
nobodyxu <Jiahao_XU@outlook.com>
2020-11-20 22:18:22 +1100

    Add ASSERT_ASPAWNF in test/utility.h

--- test/utility.h
+++ test/utility.h
@@ -3,6 +3,8 @@
 
 # include <assert.h>
 # include <stddef.h>
+# include <errno.h>
+# include <err.h>
 
 # define ASSERT_SYSCALL(expr)                                           \
     ({                                                                  \
@@ -10,6 +12,16 @@
         if (result < 0)                                                 \
             err(1, "%s on line %zu failed", # expr, (size_t) __LINE__); \
         result;                                                         \
+     })
+
+# define ASSERT_ASPAWNF(expr)        \
+    ({                               \
+        int result = (expr);         \
+        if (result < 0) {            \
+            errno = -result;         \
+            err(1, # expr "failed"); \
+        }                            \
+        result;                      \
      })
 
 int test_aspawn_fn(void *arg, int write_end_fd, void *old_sigset, void *user_data, size_t user_data_len);
